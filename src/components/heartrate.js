7d5c625833bb8242e55a438fb73d60a4c2804063
FowlFarmer <theodorez888@gmail.com>
2023-10-29 04:09:30 -0700

    heartrate

---
+++ src/components/heartrate.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+function NumberDisplay() {
+  const [number, setNumber] = useState(getRandomNumber());
+
+  useEffect(() => {
+    const updateNumber = () => {
+      setNumber(getRandomNumber());
+    };
+
+    const intervalId = setInterval(updateNumber, 2000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function getRandomNumber() {
+    const randomValue = Math.floor(Math.random() * (90)) + 60;
+    if (randomValue > 150) {
+      return "High heart rate, take a break";
+    }
+    return randomValue;
+  }
+
+  return (
+    <div>
+      <h1>{number}</h1>
+    </div>
+  );
+}
+
+export default NumberDisplay;
