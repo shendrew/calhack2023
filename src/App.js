03deecef94af670b9dc6ba60c11334445953b7d0
Cera <75736027+acrenw@users.noreply.github.com>
2023-10-29 03:56:25 -0700

    got frame to work in app.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './style.css';
-import Frame from "./components/Frame"
+// import './style.css';
+// import Frame from "./components/Frame"
 
-export default function App() {
+// export default function App() {
 
-  return (
-    <div>
-      <Frame />
-    </div>
-  );
+//   return (
+//     <div>
+//       <Frame />
+//     </div>
+//   );
 
-}
+// }
+// // my_project/app.js
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Handle form submission
+// app.post('/submit', (req, res) => {
+//     const userInput = req.body.userInput;
+//     console.log('User input:', userInput);
+// });
+
+
+// const bodyParser = require('body-parser');
+
+// // Parse form data
+// app.use(bodyParser.urlencoded({ extended: true }));
